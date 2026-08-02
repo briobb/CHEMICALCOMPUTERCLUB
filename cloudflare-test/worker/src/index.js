7baaf3c7d95e3594f8d4ad5ab5933ad71156e18d
briobb <infobriobb@gmail.com>
2026-08-03 08:29:32 +0900

    Create index.js

---
+++ cloudflare-test/worker/src/index.js
@@ -0,0 +1,21 @@
+const products = {
+  "logo-t": {
+    name: "CCC Logo T",
+    price: 4400,
+    sizes: ["S", "M", "L", "XL"]
+  },
+  mug: {
+    name: "CCC Mug",
+    price: 2200
+  },
+  bag: {
+    name: "Equipment Bag",
+    price: 2200
+  },
+  sox: {
+    name: "CCC Sox",
+    price: 2200,
+    sizes: ["S", "M", "L"],
+    colors: ["Ivory", "Navy", "Orange"]
+  }
+};
